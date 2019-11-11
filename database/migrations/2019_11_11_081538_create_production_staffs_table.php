bc08ddb2e51bd11b736fe52d593ca2039cb52ed3
theminer3746 <me@theminerdev.com>
2019-11-11 15:16:40 +0700

    Create production staffs table

---
+++ database/migrations/2019_11_11_081538_create_production_staffs_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateProductionStaffsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('production_staffs', function (Blueprint $table) {
+            $table->char('production_staff_ssn', 13);
+            $table->foreign('production_staff_ssn')->references('ssn')->on('employees')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('production_staffs');
+    }
+}
