1ec808e45360f613f741739509aef6d2a38d889d
theminer3746 <me@theminerdev.com>
2019-11-11 15:21:38 +0700

    Create customers table

---
+++ database/migrations/2019_11_11_082008_create_customers_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCustomersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('customers', function (Blueprint $table) {
+            $table->string('title', 10);
+            $table->string('fname', 30);
+            $table->string('lname', 30);
+            $table->unsignedInteger('customer_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('customers');
+    }
+}
