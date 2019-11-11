f6bd406e4c521e0fb481fe59350ca06961c0640a
theminer3746 <me@theminerdev.com>
2019-11-11 16:18:29 +0700

    Create supplier addressed table

---
+++ database/migrations/2019_11_11_091155_create_supplier_addresses_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateSupplierAddressesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('supplier_addresses', function (Blueprint $table) {
+            $table->unsignedInteger('address_id')->autoIncrement();
+            $table->unsignedInteger('supplier_id');
+            $table->foreign('supplier_id')
+                ->references('supplier_id')
+                ->on('suppliers')
+                ->onDelete('cascade');
+            $table->string('line_1', 50);
+            $table->string('line_2', 50);
+            $table->string('city', 20);
+            $table->char('country', 3);
+            $table->string('postal_code', 10);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('supplier_addresses');
+    }
+}
