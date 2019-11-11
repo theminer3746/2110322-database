51a2bc916b66a71300a48eeb62b48e7282d1afd5
theminer3746 <me@theminerdev.com>
2019-11-11 16:21:17 +0700

    Create department addresses table

---
+++ database/migrations/2019_11_11_091931_create_department_addresses_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateDepartmentAddressesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('department_addresses', function (Blueprint $table) {
+            $table->unsignedInteger('address_id')->autoIncrement();
+            $table->unsignedTinyInteger('department_id');
+            $table->foreign('department_id')
+                ->references('department_id')
+                ->on('departments')
+                ->onDelete('cascade');
+            $table->string('line_1', 50);
+            $table->string('line_2', 50);
+            $table->string('city', 20);
+            $table->char('country', 3);
+            $table->string('postal_code', 10);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('department_addresses');
+    }
+}
