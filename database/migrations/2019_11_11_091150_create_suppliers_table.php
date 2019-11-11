ebd349caf37c15a03a1ecf1f10f4327682fdb32c
theminer3746 <me@theminerdev.com>
2019-11-11 16:18:15 +0700

    Create suppliers table

---
+++ database/migrations/2019_11_11_091150_create_suppliers_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateSuppliersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('suppliers', function (Blueprint $table) {
+            $table->unsignedInteger('supplier_id')->autoIncrement();
+            $table->string('name', 30);
+            $table->string('contact', 12);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('suppliers');
+    }
+}
