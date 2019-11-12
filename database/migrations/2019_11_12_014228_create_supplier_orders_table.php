662e5157f83a95092dfef867f1126bb2f4cffdd2
theminer3746 <me@theminerdev.com>
2019-11-12 08:47:50 +0700

    Create supplier orders table migration

---
+++ database/migrations/2019_11_12_014228_create_supplier_orders_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateSupplierOrdersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('supplier_orders', function (Blueprint $table) {
+            $table->unsignedInteger('order_id')->autoIncrement();
+            $table->timestamp('order_date');
+            $table->timestamp('payment_date');
+            $table->string('payment_method', 10);
+            $table->unsignedInteger('total_price');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('supplier_orders');
+    }
+}
