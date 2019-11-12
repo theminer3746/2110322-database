577e007399fa87d8f2a78e4570bac0002ed8cbe5
theminer3746 <me@theminerdev.com>
2019-11-12 08:53:13 +0700

    Create customer orders migration

---
+++ database/migrations/2019_11_12_015014_create_customer_orders_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCustomerOrdersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('customer_orders', function (Blueprint $table) {
+            $table->unsignedInteger('order_id')->autoIncrement();
+            $table->timestamp('order_date');
+            $table->unsignedInteger('total_price');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('customer_orders');
+    }
+}
