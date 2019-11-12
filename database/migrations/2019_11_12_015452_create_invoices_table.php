f2e59184d250e15b67ae3d19f7586016d5f39ad9
theminer3746 <me@theminerdev.com>
2019-11-12 08:58:11 +0700

    Create invoices table migration

---
+++ database/migrations/2019_11_12_015452_create_invoices_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateInvoicesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('invoices', function (Blueprint $table) {
+            $table->unsignedInteger('invoice_id')->autoIncrement();
+            $table->timestamp('paid_at');
+            $table->string('payment_method', 20);
+            $table->string('additional_information', 2000);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('invoices');
+    }
+}
