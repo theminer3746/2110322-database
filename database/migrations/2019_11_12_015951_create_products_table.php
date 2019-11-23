4ac92b9161b46c484eaadc878f619867ebcdbae9
theminer3746 <me@theminerdev.com>
2019-11-23 17:36:57 +0700

    Create product table migration

---
+++ database/migrations/2019_11_12_015951_create_products_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->string('product_name', 20);
+            $table->string('product_id', 15)->primary();
+            $table->unsignedMediumInteger('price');
+            $table->string('information', 2000);
+            $table->unsignedMediumInteger('amount');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}
