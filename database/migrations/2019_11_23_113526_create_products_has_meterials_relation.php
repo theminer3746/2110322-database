095d16616fcb7eee5055789074a820d0248844c9
theminer3746 <me@theminerdev.com>
2019-11-23 18:47:05 +0700

    Create products has materials relation

---
+++ database/migrations/2019_11_23_113526_create_products_has_meterials_relation.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateProductsHasMeterialsRelation extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products_has_meterials_relation', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedSmallInteger('material_id');
+            $table->foreign('material_id')
+                ->references('material_id')
+                ->on('materials')
+                ->onUpdate('cascade');
+            $table->string('product_id', 15);
+            $table->foreign('product_id')
+                ->references('product_id')
+                ->on('products')
+                ->onUpdate('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products_has_meterials_relation');
+    }
+}
