a2d77ec5154d91bcabd8f15758cd43d1b7b32571
theminer3746 <me@theminerdev.com>
2019-11-23 17:50:57 +0700

    Add customer pays invoice and invoice bills to customer's address relation

--- database/migrations/2019_11_12_015452_create_invoices_table.php
+++ database/migrations/2019_11_12_015452_create_invoices_table.php
@@ -18,6 +18,16 @@ class CreateInvoicesTable extends Migration
             $table->timestamp('paid_at');
             $table->string('payment_method', 20);
             $table->string('additional_information', 2000);
+            $table->unsignedInteger('customer_id');
+            $table->foreign('customer_id')
+                ->references('customer_id')
+                ->on('customers')
+                ->onDelete('cascade');
+            $table->unsignedInteger('address_id');
+            $table->foreign('address_id')
+                ->references('address_id')
+                ->on('customer_addresses')
+                ->onDelete('cascade');
             $table->timestamps();
         });
     }
